34867172a3756de6e9eafac4b8bb1108ee6f12ac
mohandast52 <mohandast52@gmail.com>
2023-03-31 19:33:40 +0530

    add back mock

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,18 @@
+/* eslint-disable jest/require-hook */
 import '@testing-library/jest-dom/extend-expect';
 import '@testing-library/jest-dom';
+
+// https:// jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
