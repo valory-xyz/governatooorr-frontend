3b76d79be9856af78faa6c103c927e1b302cb49a
Oaksprout <oaksproutthetan@gmail.com>
2023-04-06 12:16:26 +0200

    update meta tag info

--- util/constants.jsx
+++ util/constants.jsx
@@ -6,9 +6,9 @@ export const MORE_COLOR = {
   SECONDARY_GRAY: '#f0f0f0',
 };
 
-export const SITE_URL = '';
+export const SITE_URL = 'https://governatooorr.autonolas.network';
 export const SITE_TITLE = 'Governatooorr';
-export const SITE_DESCRIPTION = 'Delegate your voting power to an autonomous AI. Have it vote for good or evil.';
+export const SITE_DESCRIPTION = 'Delegate your voting power to an autonomous AI. Have it vote for good or evil. 🤪';
 export const SITE_METATAG_IMAGE = `${SITE_URL}/images/site-metadata/site-metatag.jpg`;
 
 export const META_TAGS_INFO = {
