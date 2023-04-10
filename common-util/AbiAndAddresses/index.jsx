55dc1a88b5e28492c21872f7fa1a552dcd2d94a8
mohandast52 <mohandast52@gmail.com>
2023-04-11 05:22:22 +0530

    remove default erc20 token

--- common-util/AbiAndAddresses/index.jsx
+++ common-util/AbiAndAddresses/index.jsx
@@ -1,3 +1,2 @@
 export * from './mintNftContractGoerli';
 export * from './mintNftContractMainnet';
-export * from './defaultErc20Abi';
--- components/Delegate/utils.jsx
+++ components/Delegate/utils.jsx
@@ -1,10 +1,8 @@
 import { uniqBy } from 'lodash';
 import { gql } from '@apollo/client';
-
 import axios from 'axios';
 import { getWeb3Details } from 'common-util/Contracts';
 import { SUPPORTED_TOKEN_TYPES, ACCEPTED_GOVERNOR_TYPES } from 'util/constants';
-import { DEFAULT_ERC20_ABI } from 'common-util/AbiAndAddresses';
 
 export const QUERY = gql`
   query Governors(
@@ -87,56 +85,10 @@ const getContractAddress = async (tokenAddress) => {
   throw new Error('No Proxy contract address found');
 };
 
-// create a function and get proxy contract using token address through etherscan api
-export const getContractProxyAbi = async (tokenAddress) => {
-  console.log('getContractProxyAbi START');
-  const API = `https://api.etherscan.io/api?module=contract&action=getsourcecode&address=${tokenAddress}&apikey=${process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY}`;
-  // const API = `https://api.etherscan.io/api?module=proxy&action=eth_call&to=${tokenAddress}&data=0x3570b2c0&apikey=${process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY}`;
-  try {
-    const response = await axios.get(API);
-    if (response.status !== 200) {
-      throw new Error(
-        `Error retrieving token contract ABI: Status ${response.status}`,
-      );
-    }
-    const stringedTokenContractAbi = response.data.result[0];
-    console.log(stringedTokenContractAbi);
-    // if (!stringedTokenContractAbi) {
-    //   console.warn(
-    //     'No contract ABI available to retrieve. Falling back to default ABI.',
-    //   );
-    //   return DEFAULT_ERC20_ABI;
-    // }
-    const parsedAbi = JSON.parse(stringedTokenContractAbi);
-    // https://stackoverflow.com/a/74771952
-    // console.log(parsedAbi);
-
-    if (stringedTokenContractAbi.includes('implementation')) {
-      // const hasBalanceOf = parsedAbi.find((entry) => entry.name === 'implementation');
-      const { web3 } = getWeb3Details();
-      const contract = new web3.eth.Contract(parsedAbi, tokenAddress);
-      const abc = await contract.methods.implementation().send({
-        from: '0xe3D1fB73D21895286aef7063444d173626eb9C9E',
-      });
-      console.log(abc);
-      // const abc = await web3.eth.getTransactionReceipt(
-      //   tokenAddress,
-      // );
-      // console.log(abc);
-    }
-
-    return parsedAbi;
-  } catch (e) {
-    console.log(e);
-  }
-  console.log('getContractProxyAbi END');
-  return null;
-};
-
 const getTokenContractAbi = async (tokenAddress) => {
-  const etherscanApiUrl = `https://api.etherscan.io/api?module=contract&action=getabi&address=${tokenAddress}&apikey=${process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY}`;
-
   try {
+    const address = await getContractAddress(tokenAddress);
+    const etherscanApiUrl = `${ETHERSCAN_API}?module=contract&action=getabi&address=${address}&apikey=${process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY}`;
     const response = await axios.get(etherscanApiUrl);
 
     if (response.status !== 200) {
@@ -146,43 +98,7 @@ const getTokenContractAbi = async (tokenAddress) => {
     }
 
     const stringedTokenContractAbi = response.data.result;
-    console.log(stringedTokenContractAbi);
-    if (!stringedTokenContractAbi) {
-      console.warn(
-        'No contract ABI available to retrieve. Falling back to default ABI.',
-      );
-      return DEFAULT_ERC20_ABI;
-    }
-
-    await getContractProxyAbi(tokenAddress);
     const parsedAbi = JSON.parse(stringedTokenContractAbi);
-
-    const hasBalanceOf = parsedAbi.some((entry) => entry.name === 'balanceOf');
-    const hasDelegate = parsedAbi.some((entry) => entry.name === 'delegate');
-
-    if (!hasBalanceOf) {
-      console.warn('Using default ERC20 ABI due to missing balanceOf function');
-      return DEFAULT_ERC20_ABI;
-    }
-
-    if (!hasDelegate) {
-      console.warn('Using default ERC20 ABI due to missing delegate function');
-      return DEFAULT_ERC20_ABI;
-    }
-
-    // if (stringedTokenContractAbi.includes('implementation')) {
-    //   // const hasBalanceOf = parsedAbi.find((entry) => entry.name === 'implementation');
-    //   const { web3 } = getWeb3Details();
-    //   const contract = new web3.eth.Contract(parsedAbi, tokenAddress);
-
-    //   console.log(contract);
-
-    //   const abc = await contract.methods
-    //     .implementation()
-    //     .send({ from: '0xe3D1fB73D21895286aef7063444d173626eb9C9E' });
-    //   console.log(abc);
-    // }
-
     return parsedAbi;
   } catch (e) {
     console.error('Error retrieving token contract ABI:', e);
