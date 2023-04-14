74899aac32594821e3c6ef78f722bcf7e500398f
mohandast52 <mohandast52@gmail.com>
2023-04-15 01:36:53 +0530

    check if the events is working fine

--- components/Delegate/utils.jsx
+++ components/Delegate/utils.jsx
@@ -1,8 +1,12 @@
-import { uniqBy } from 'lodash';
 import { gql } from '@apollo/client';
 import axios from 'axios';
+import { uniqBy, get } from 'lodash';
 import { getWeb3Details } from 'common-util/Contracts';
-import { SUPPORTED_TOKEN_TYPES, ACCEPTED_GOVERNOR_TYPES } from 'util/constants';
+import {
+  SUPPORTED_TOKEN_TYPES,
+  ACCEPTED_GOVERNOR_TYPES,
+  DELEGATEE_ADDRESS,
+} from 'util/constants';
 
 const ETHERSCAN_URL = 'https://api.etherscan.io/api';
 
@@ -76,6 +80,8 @@ const getContractAddress = async (tokenAddress) => {
     );
     const proxyAddress = response.data.result[0].Implementation;
 
+    console.log({ proxyAddress });
+
     // if proxyAddress is null, then the contract is not a proxy contract
     return proxyAddress || tokenAddress;
   } catch (error) {
@@ -94,14 +100,17 @@ const getTokenContractAbi = async (tokenAddress) => {
       `${ETHERSCAN_URL}?module=contract&action=getabi&address=${address}&apikey=${process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY}`,
     );
 
-    if (response.status !== 200) {
+    if (response.status !== 200 && response.data.status !== '1') {
       throw new Error(
         `Error retrieving token contract ABI: Status ${response.status}`,
       );
     }
 
+    console.log(response);
+
     const stringedTokenContractAbi = response.data.result;
     const parsedAbi = JSON.parse(stringedTokenContractAbi);
+    console.log(parsedAbi);
     return parsedAbi;
   } catch (e) {
     console.error('Error retrieving token contract ABI:', e);
@@ -114,13 +123,14 @@ export const getFullTokenContractAbi = async (subTokenAddress) => {
   return stringedTokenContractAbi;
 };
 
-export const createTokenContract = (token) => {
+export const createTokenContract = (abi, token) => {
   const { web3 } = getWeb3Details();
 
   // Check if tokenContractAbi is not empty
-  if (token) {
-    return new web3.eth.Contract(token);
+  if (abi && token) {
+    return new web3.eth.Contract(abi, token);
   }
+
   throw new Error('Token contract ABI is empty');
 };
 
@@ -130,3 +140,47 @@ export const getUniqueGovernorBravoGovernors = (governors) => {
 };
 
 export const isSupportedTokenType = (token) => SUPPORTED_TOKEN_TYPES.includes(token.type);
+
+/**
+ * Contract call to delegate tokens
+ */
+export const delegateTokensRequest = ({
+  account,
+  tokenContractAbi,
+  tokenAddress,
+  // others
+}) => new Promise((resolve, reject) => {
+  const contract = createTokenContract(tokenContractAbi, tokenAddress);
+
+  contract.methods
+    .delegate(DELEGATEE_ADDRESS)
+    .send({ from: account })
+    .then((response) => {
+      // const id = get(response, 'events.Transfer.returnValues.id');
+      // resolve(id);
+      window.console.log(
+        { response },
+      );
+      // listen to the delegate event & check if the delegatee address is correct
+      contract.events
+        .DelegateChanged()
+        .on('data', (event) => {
+          window.console.log('Transfer event data:', event);
+          const { returnValues } = event;
+          const { delegatee } = returnValues;
+          if (delegatee === DELEGATEE_ADDRESS) {
+            resolve(true);
+          }
+        })
+        .on('error', (error) => {
+          window.console.log(
+            'Error occurred when listening to the Transfer event',
+          );
+          reject(error);
+        });
+    })
+    .catch((e) => {
+      window.console.log('Error occurred when delegating tokens');
+      reject(e);
+    });
+});
